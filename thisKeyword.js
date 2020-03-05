097f8d57f4a1e73fbcc0a0ac2da1a2fcc32b1971
jhavivek1996 <jhavivek260@gmail.com>
2020-03-05 18:11:33 +0530

    5 march Commit

---
+++ thisKeyword.js
@@ -0,0 +1,19 @@
+let user = {
+       
+  
+    "id":1,
+    "name":"Satish",
+    "surname":"Pal",
+    "designation":"Python Developer",
+    "place":"Nadiad",
+    "college":"KJIT",
+    "hobby":"Travelling",
+     sayHii:function(){
+         console.log(`Hii, my friend name is ${this.name}.`)
+     }
+    
+
+
+};
+
+user.sayHii();
