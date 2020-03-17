18f21053ca4356c1194e308807376e06cb10c44e
jhavivek1996 <jhavivek260@gmail.com>
2020-03-17 18:32:09 +0530

    Vue Js Example

---
+++ demo/program.py
@@ -0,0 +1,14 @@
+class Ashutosh:
+	def rock(self,name,age):
+		return name,age
+
+class krunal(Ashutosh):
+	def rr(self):
+		return "I am Krunal"
+Ash =krunal()
+print(Ash.rr())
+print(Ash.rock("Vivek",23))
+
+
+for i in range(1,10):
+	print(i)
